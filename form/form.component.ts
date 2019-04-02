63f89ddacf7957711c7cbd5206691b2c2a018dc0
Shantanu <42547471+Bonzi111@users.noreply.github.com>
2019-04-02 13:00:05 +0530

    Update form.component.ts

--- form/form.component.ts
+++ form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,36 +7,25 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-private check :boolean;
-  constructor() {
-  }
-  reactiveForm = new FormGroup({
-    AV: new FormControl('', [Validators.required,Validators.min(0), Validators.max(100)]),
-    about:new FormControl('',Validators.required),
-    BN: new FormControl('',Validators.required),
-    FG: new FormControl('', [Validators.required,Validators.min(0), Validators.max(100)]),
-    IBUS: new FormControl('', [Validators.required,Validators.min(0), Validators.max(100)]),
-    SRM:new FormControl('', [Validators.required,Validators.min(0), Validators.max(100)]),
-    cat_id: new FormControl('', Validators.required),
-    company: new FormControl('', Validators.required),
-    established:new FormControl('',Validators.required),
-    features: new FormControl('', Validators.required),
-    website:new FormControl('',Validators.required),   
-  });
-  hide(err){
-    this.check =true;
-    if(){
-      
-    }
-  }
-  ngOnInit() {
-  }
-  get f() {
-    return this.reactiveForm.controls;
-  }
-  onSubmit() {
-    alert("Submitted")
-    console.warn(this.reactiveForm.value);
-  }
+  rForm: FormGroup;
+ 
 
+  constructor(fb: FormBuilder){
+    this.rForm = fb.group({
+      'category': [null, Validators.required],
+      'comapny': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'IBUS': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'features': [null,[Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'AV': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'FG': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'SRM': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'BN': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'about': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'website': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]],
+      'established': [null, [Validators.required,Validators.pattern("^[A-Za-z]+$")]]
+    });
+  }
+  ngOnInit(){
+
+  }
 }
